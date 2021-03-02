d30acf2d1caaddec8d915cff6d0463792f5639e9
James Cole <james@firefly-iii.org>
2021-03-02 18:23:04 +0100

    New descriptions.

--- index.php
+++ index.php
@@ -3,45 +3,77 @@ declare(strict_types=1);
 
 use ApiDocBuilder\Builder\Builder;
 
+// default values:
+$version     = '0.1-beta';
+$destination = '/';
+$tags        = [];
+
 include 'vendor/autoload.php';
 include 'config.php';
 
-$builder = new Builder(ROOT . '/templates', ROOT . '/cache');
+$builder = new Builder(sprintf('%s/templates', ROOT), sprintf('%s/cache', ROOT));
 $builder->setVersion($version);
 
-// add all tags to the yaml file.
-foreach ($tags as $name => $description) {
-    $builder->addTag($name, $description);
+// add tags
+/**
+ * @var string $name
+ * @var array  $info
+ */
+foreach ($tags as $name => $info) {
+    $builder->addTag($name, $info['description']);
 }
 
-// add all paths to the final YAML file by simply looping the directory:
-$files = scandir(ROOT . '/yaml/paths', SCANDIR_SORT_ASCENDING);
-foreach ($files as $file) {
-    if (!\in_array($file, ['.', '..','.DS_Store'])) {
-        if ('yaml' === substr($file, -4)) {
-            echo sprintf("Add %s\n", $file);
-            $builder->addYamlFile('paths', ROOT . '/yaml/paths/' . $file, 1);
+// scan directories and add them!
+$directories = [
+    'yaml/autocomplete',
+];
+
+foreach ($directories as $directory) {
+
+    // list all files in the directory:
+    $fullDirectory = sprintf('%s/%s', ROOT, $directory);
+    $files         = scandir($fullDirectory, SCANDIR_SORT_ASCENDING);
+
+    // loop al files in this directory:
+    foreach ($files as $file) {
+        // must be YAML file.
+        $fullPath = sprintf('%s/%s', $fullDirectory, $file);
+
+        // add to thing:
+        if ('yaml' === substr($fullPath, -4)) {
+            //echo sprintf("Adding file %s\n", $fullPath);
+            $builder->addYamlFile('paths', $fullPath, 1);
         }
     }
 }
 
 // add all models to the final YAML file by also looping the directory:
-
-$files = scandir('./yaml/schemas', SCANDIR_SORT_ASCENDING);
+$fullDirectory = sprintf('%s/yaml/schemas', ROOT);
+$files         = scandir($fullDirectory, SCANDIR_SORT_ASCENDING);
 foreach ($files as $file) {
-    if (!\in_array($file, ['.', '..','.DS_Store'])) {
-        echo sprintf("Add %s\n", $file);
+    $fullPath = sprintf('%s/%s', $fullDirectory, $file);
+    if ('yaml' === substr($fullPath, -4)) {
+        //echo sprintf("Add %s\n", $file);
         $builder->addYamlFile('schemas', ROOT . '/yaml/schemas/' . $file, 2);
     }
 }
 
 
-$result = $builder->render();
+//$files = scandir(ROOT . '/yaml/paths', SCANDIR_SORT_ASCENDING);
 
+$result = $builder->render();
+echo '<pre>';
+echo $result;
+// put in file:
 // put result in file:
 $finalDestination = sprintf('%s/firefly-iii-%s.yaml', $destination, $version);
 
 file_put_contents($finalDestination, $result);
-echo "\nDone\n";
+
+exit;
+
+
+
+
 
 
