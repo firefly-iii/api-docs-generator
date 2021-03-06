096bdfebf4d236ba91f73fa2083b154d9c2bc82e
James Cole <james@firefly-iii.org>
2021-03-06 12:28:42 +0100

    Fix category

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $dotenv->load();
 
 $version     = '1.5.0';
 $destination = getenv('API_DESTINATION');
-$server = getenv('API_SERVER');
+$server      = getenv('API_SERVER');
 
 define('ROOT', getenv('API_ROOT'));
 
@@ -26,31 +26,31 @@ $tags = [
 
     // models
     'accounts'          => ['description' => 'Endpoints that deliver all of the user\'s asset, expense and other accounts (and the metadata) together with related transactions, piggy banks and other objects. Also delivers endpoints for CRUD operations for accounts.'],
-//    'attachments'       => ['description' => 'Endpoints to manage the attachments of the authenticated user, including up- and downloading of the files.'],
-//    'available_budgets' => ['description' => 'Endpoints to manage the total available amount that the user has made available to themselves. Used in periodic budgeting.'],
-//    'bills'             => ['description' => 'Endpoints to manage a user\'s bills and all related objects.'],
-//    'budgets'           => ['description' => 'Endpoints to manage a user\;s budgets and get info on the related objects, like limits.'],
-//    'categories'        => ['description' => 'Endpoints to manage a user\'s categories and get information on transactions and other related objects.'],
-//    'currencies'        => ['description' => 'Endpoints to manage the currencies in Firefly III. Depending on the user\'s role you can also disable and enable them, or add new ones.'],
-//    'links'             => ['description' => 'Endpoints to manage links between transactions, and manage the type of links available.'],
-//    'piggy_banks'       => ['description' => 'Endpoints to control and manage all of the user\'s piggy banks and related objects and information.'],
-//    'recurrences'       => ['description' => 'Use these endpoints to manage the user\'s recurring transactions, trigger the creation of transactions and manage the settings.'],
-//    'rules'             => ['description' => 'These endpoints can be used to manage all of the user\'s rules. Also includes triggers to execute or test rules and individual triggers.'],
-//    'rule_groups'       => ['description' => 'Manage all of the user\'s groups of rules and trigger the execution of entire groups.'],
-//    'tags'              => ['description' => 'This endpoint manages all of the user\'s tags.'],
-//    'transactions'      => ['description' => 'The most-used endpoints in Firefly III, these endpoints are used to manage the user\'s transactions.'],
-//
-//    // search
-//    'search'            => ['description' => 'Endpoints that allow you to search through the user\'s financial data. Different from the autocomplete endpoints, the search accepts more advanced arguments.'],
-//
-//    // system
-//    'about'             => ['description' => 'These endpoints deliver general system information, version- and meta information.'],
-//    'configuration'     => ['description' => 'These endpoints allow you to manage and update the Firefly III configuration'],
-//    'users'             => ['description' => 'Use these endpoints to manage the users registered within Firefly III. You need to have the "owner" role to access these endpoints.'],
-//
-//    // user
-//    'destruction'       => ['description' => 'This endpoint allows you to mass-destroy user data.',],
-//    'preferences'       => ['description' => 'These endpoints can be used to manage the user\'s preferences, including some hidden ones.'],
-//    'webhooks'          => ['description' => 'These endpoints can be used to manage the user\'s webhooks and triggers them if necessary.'],
+    'attachments'       => ['description' => 'Endpoints to manage the attachments of the authenticated user, including up- and downloading of the files.'],
+    'available_budgets' => ['description' => 'Endpoints to manage the total available amount that the user has made available to themselves. Used in periodic budgeting.'],
+    'bills'             => ['description' => 'Endpoints to manage a user\'s bills and all related objects.'],
+    'budgets'           => ['description' => 'Endpoints to manage a user\;s budgets and get info on the related objects, like limits.'],
+    'categories'        => ['description' => 'Endpoints to manage a user\'s categories and get information on transactions and other related objects.'],
+    'currencies'        => ['description' => 'Endpoints to manage the currencies in Firefly III. Depending on the user\'s role you can also disable and enable them, or add new ones.'],
+    'links'             => ['description' => 'Endpoints to manage links between transactions, and manage the type of links available.'],
+    'piggy_banks'       => ['description' => 'Endpoints to control and manage all of the user\'s piggy banks and related objects and information.'],
+    'recurrences'       => ['description' => 'Use these endpoints to manage the user\'s recurring transactions, trigger the creation of transactions and manage the settings.'],
+    'rules'             => ['description' => 'These endpoints can be used to manage all of the user\'s rules. Also includes triggers to execute or test rules and individual triggers.'],
+    'rule_groups'       => ['description' => 'Manage all of the user\'s groups of rules and trigger the execution of entire groups.'],
+    'tags'              => ['description' => 'This endpoint manages all of the user\'s tags.'],
+    'transactions'      => ['description' => 'The most-used endpoints in Firefly III, these endpoints are used to manage the user\'s transactions.'],
+    //
+    //    // search
+    //    'search'            => ['description' => 'Endpoints that allow you to search through the user\'s financial data. Different from the autocomplete endpoints, the search accepts more advanced arguments.'],
+    //
+    //    // system
+    //    'about'             => ['description' => 'These endpoints deliver general system information, version- and meta information.'],
+    //    'configuration'     => ['description' => 'These endpoints allow you to manage and update the Firefly III configuration'],
+    //    'users'             => ['description' => 'Use these endpoints to manage the users registered within Firefly III. You need to have the "owner" role to access these endpoints.'],
+    //
+    //    // user
+    //    'destruction'       => ['description' => 'This endpoint allows you to mass-destroy user data.',],
+    //    'preferences'       => ['description' => 'These endpoints can be used to manage the user\'s preferences, including some hidden ones.'],
+    //    'webhooks'          => ['description' => 'These endpoints can be used to manage the user\'s webhooks and triggers them if necessary.'],
 ];
 
