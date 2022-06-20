7994832d1f264d614304cb3147989ffdc3c75133
James Cole <james@firefly-iii.org>
2022-06-20 08:23:32 +0200

    Update paths and render routine

--- index.php
+++ index.php
@@ -5,17 +5,21 @@ use ApiDocBuilder\Builder\Builder;
 
 // default values:
 $version     = '0.1-beta';
+$pathVersion = 'v1';
 $server      = 'https://demo.firefly-iii.org';
-$destination = '/';
+$destination = './';
 $tags        = [];
 
 include 'vendor/autoload.php';
 include 'config.php';
 
-$builder = new Builder(sprintf('%s/templates', ROOT), sprintf('%s/cache', ROOT));
+$templatesDir = sprintf('%s/templates', ROOT);
+$cacheDir     = sprintf('%s/cache', ROOT);
+
+$builder = new Builder($templatesDir, $cacheDir);
 $builder->setVersion($version);
+$builder->setPathVersion($pathVersion);
 $builder->setServer($server);
-//echo '<pre>';
 
 // add tags
 /**
@@ -30,32 +34,17 @@ unset($name);
 // scan directories and add all paths:
 $directories = [
     [
-        'path'        => 'yaml/v1/paths',
+        'path'        => sprintf('yaml/%s/paths', $pathVersion),
         'identifier'  => 'paths',
         'indentation' => 1,
     ],
     [
-        'path'        => 'yaml/v2/paths',
-        'identifier'  => 'paths',
-        'indentation' => 1,
-    ],
-
-    [
-        'path'        => 'yaml/v1/schemas',
+        'path'        => sprintf('yaml/%s/schemas', $pathVersion),
         'identifier'  => 'schemas',
         'indentation' => 2,
     ],
-
-    [
-        'path'        => 'yaml/v2/schemas',
-        'identifier'  => 'schemas',
-        'indentation' => 2,
-    ],
-
-
 ];
 
-/** @var array $info */
 foreach ($directories as $info) {
 
     // list all files in the directory:
@@ -67,27 +56,14 @@ foreach ($directories as $info) {
      */
     foreach ($objects as $fullPath => $object) {
         // add to thing:
-        if ('yaml' === substr($fullPath, -4)) {
-            //echo sprintf("Added %s\n", $fullPath);
-
-            //echo sprintf("Adding file %s\n", $fullPath);
+        if (str_ends_with($fullPath, 'yaml')) {
             $builder->addYamlFile($info['identifier'], $fullPath, $info['indentation']);
         }
     }
 }
 
-$result = $builder->render();
-//echo $result;
-// put in file:
-// put result in file:
-$finalDestination = sprintf('%s/firefly-iii-%s.yaml', $destination, $version);
+$result           = $builder->render();
+$finalDestination = sprintf('%s/firefly-iii-%s-%s.yaml', $destination, $pathVersion, $version);
 
 file_put_contents($finalDestination, $result);
-
-exit;
-
-
-
-
-
 
