55ef026a7a5b8146c56e19e843eda593410be244
James Cole <james@firefly-iii.org>
2023-09-01 05:14:35 +0200

    Add missing fields for https://github.com/dvankley/firefly-plaid-connector-2/issues/15

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $dotenv->load();
 
 $destination = getenv('API_DESTINATION');
 $server      = getenv('API_SERVER');
-$version     = '2.0.5';
+$version     = '2.0.6';
 
 define('ROOT', getenv('API_ROOT'));
 
