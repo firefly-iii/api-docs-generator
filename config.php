3ba6a5980b7e6f2dae402e131a4285c1bbc7ac34
James Cole <james@firefly-iii.org>
2023-09-01 19:55:58 +0200

    v2.0.7

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $dotenv->load();
 
 $destination = getenv('API_DESTINATION');
 $server      = getenv('API_SERVER');
-$version     = '2.0.6';
+$version     = '2.0.7';
 
 define('ROOT', getenv('API_ROOT'));
 
