26f5f1d77d7b4e153907966663acedae235d6858
James Cole <james@firefly-iii.org>
2023-09-09 14:36:20 +0200

    Add empty required array, bump version.

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $dotenv->load();
 
 $destination = getenv('API_DESTINATION');
 $server      = getenv('API_SERVER');
-$version     = '2.0.7';
+$version     = '2.0.8';
 
 define('ROOT', getenv('API_ROOT'));
 
